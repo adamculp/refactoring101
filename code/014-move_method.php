2e80e0c40ccdaaa999e72bd9263ba98e048d1f7d
adamculp <adamculp@uws.net>
2022-04-03 14:28:49 -0400

    Updated comments, added types to bring to PHP version 7 level. Updated visibility of fields in classes. Refactored CHILDREN constant.
    Signed-off-by: adamculp <adamculp@uws.net>

--- code/014-move_method.php
+++ code/014-move_method.php
@@ -1,5 +1,8 @@
 <?php
 /**
+ * We change the Price class to no longer be abstract, and remove the getPriceCode()
+ * abstract method.
+ *
  * In this step we "Move Method" getCharge() from Movie to Price and call it from
  * the old getCharge() left behind in the Movie class.
  * 
@@ -8,9 +11,11 @@
  * 
  */
 
+namespace Refactoring014;
+
 class Customer {
-    public $name;
-    public $rentals;
+    protected string $name;
+    protected array $rentals;
     
     public function __construct($name) {
         $this->name = $name;
@@ -20,11 +25,11 @@ class Customer {
         $this->rentals[] = $rental;
     }
     
-    public function getName() {
+    public function getName(): string {
         return $this->name;
     }
     
-    public function statementText() {
+    public function statementText(): string {
         $result = "Rental Record for " . $this->getName() . "\n";
         
         foreach ($this->rentals as $rental) {
@@ -38,8 +43,8 @@ class Customer {
         return $result;
     }
 
-    public function statementHtml() {
-        $result = "<h1>Rental Record for <em>" . $this->getName() . "</em></h1><p>\n";
+    public function statementHtml(): string {
+        $result = "<h1>Rental Record for <em>" . $this->getName() . "</em></h1>\n";
 
         foreach ($this->rentals as $rental) {
             $result .= $rental->movie->getTitle() . " " . $rental->getCharge() . "<br />\n";
@@ -52,7 +57,7 @@ class Customer {
         return $result;
     }
     
-    public function getTotalFrequentRenterPoints() {
+    public function getTotalFrequentRenterPoints(): int {
         $result = 0;
         
         foreach ($this->rentals as $rental) {
@@ -62,7 +67,7 @@ class Customer {
         return $result;
     }
     
-    public function getTotalCharge() {
+    public function getTotalCharge(): float {
         $result = 0;
         
         foreach ($this->rentals as $rental) {
@@ -74,19 +79,19 @@ class Customer {
 }
 
 class Movie {
-    const CHILDRENS = 2;
+    const CHILDREN = 2;
     const REGULAR = 0;
     const NEW_RELEASE = 1;
     
-    public $title;
-    public $price;
+    protected string $title;
+    protected Price $price;
     
     public function __construct($title, $priceCode) {
         $this->title = $title;
         $this->setPrice($priceCode);
     }
     
-    public function getPriceCode() {
+    public function getPriceCode(): int {
         return $this->price->getPriceCode();
     }
     
@@ -97,8 +102,8 @@ class Movie {
                 $this->price = new RegularPrice();
                 break;
 
-            case self::CHILDRENS:
-                $this->price = new ChildrensPrice();
+            case self::CHILDREN:
+                $this->price = new ChildrenPrice();
                 break;
 
             case self::NEW_RELEASE:
@@ -111,54 +116,56 @@ class Movie {
         }
     }
     
-    public function getTitle() {
+    public function getTitle(): string {
         return $this->title;
     }
 
-    public function getCharge($daysRented) {
+    public function getCharge($daysRented): float {
         return $this->price->getCharge($daysRented);
     }
 
-    public function getFrequentRenterPoints($daysRented) {
-        // add bonus for a two day release rental
+    public function getFrequentRenterPoints($daysRented): int {
+        $result = 0;
+
+        // add bonus for a two-day new release rental
         if (($this->getPriceCode() == self::NEW_RELEASE) && ($daysRented > 1)) {
-            return 2;
+            $result += 2;
         } else {
-            return 1;
+            $result += 1;
         }
+
+        return $result;
     }
 }
 
 class Rental {
-    public $movie;
-    public $daysRented;
+    public Movie $movie;
+    protected int $daysRented;
     
     public function __construct(Movie $movie, $daysRented) {
         $this->movie = $movie;
         $this->daysRented = $daysRented;
     }
     
-    public function getDaysRented() {
+    public function getDaysRented(): int {
         return $this->daysRented;
     }
     
-    public function getMovie() {
+    public function getMovie(): Movie {
         return $this->movie;
     }
 
-    public function getCharge() {
+    public function getCharge(): float {
         return $this->movie->getCharge($this->getDaysRented());
     }
 
-    public function getFrequentRenterPoints() {
+    public function getFrequentRenterPoints(): int {
         return $this->movie->getFrequentRenterPoints($this->getDaysRented());
     }
 }
 
-abstract class Price {
-    abstract protected function getPriceCode();
-
-    public function getCharge($daysRented) {
+class Price {
+    public function getCharge($daysRented): float {
         $result = 0;
 
         switch ($this->getPriceCode()) {
@@ -173,7 +180,7 @@ abstract class Price {
                 $result += $daysRented * 3;
                 break;
 
-            case Movie::CHILDRENS:
+            case Movie::CHILDREN:
                 $result += 1.5;
                 if ($daysRented > 3) {
                     $result += ($daysRented - 3) * 1.5;
@@ -185,20 +192,20 @@ abstract class Price {
     }
 }
 
-class ChildrensPrice extends Price {
-    public function getPriceCode() {
-        return Movie::CHILDRENS;
+class ChildrenPrice extends Price {
+    public function getPriceCode(): int {
+        return Movie::CHILDREN;
     }
 }
 
 class NewReleasePrice extends Price {
-    public function getPriceCode() {
+    public function getPriceCode(): int {
         return Movie::NEW_RELEASE;
     }
 }
 
 class RegularPrice extends Price {
-    public function getPriceCode() {
+    public function getPriceCode(): int {
         return Movie::REGULAR;
     }
 }
