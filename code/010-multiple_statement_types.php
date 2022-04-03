ab5c2be3951fe82029bcf6ce8bf31868b405bb54
adamculp <adamculp@uws.net>
2022-04-03 14:26:52 -0400

    Updated comments, added types to bring to PHP version 7 level. Updated visibility of fields in classes. Refactored CHILDREN constant.
    Signed-off-by: adamculp <adamculp@uws.net>

--- code/010-multiple_statement_types.php
+++ code/010-multiple_statement_types.php
@@ -7,12 +7,17 @@
  * Because the calculations of the rental charges, frequent renter points, and
  * grand total are now external to the statement creation there is less duplication
  * to have two representations of the statement.
- * 
+ *
+ * NOTE: It is bad to mix HTML in strings. Separation of concerns is recommended. But
+ * for demonstration purposes, please overlook this.
+ *
  */
 
+namespace Refactoring010;
+
 class Customer {
-    public $name;
-    public $rentals;
+    protected string $name;
+    protected array $rentals;
     
     public function __construct($name) {
         $this->name = $name;
@@ -22,11 +27,11 @@ class Customer {
         $this->rentals[] = $rental;
     }
     
-    public function getName() {
+    public function getName(): string {
         return $this->name;
     }
     
-    public function statementText() {
+    public function statementText(): string {
         $result = "Rental Record for " . $this->getName() . "\n";
         
         foreach ($this->rentals as $rental) {
@@ -40,8 +45,8 @@ class Customer {
         return $result;
     }
 
-    public function statementHtml() {
-        $result = "<h1>Rental Record for <em>" . $this->getName() . "</em></h1><p>\n";
+    public function statementHtml(): string {
+        $result = "<h1>Rental Record for <em>" . $this->getName() . "</em></h1>\n";
 
         foreach ($this->rentals as $rental) {
             $result .= $rental->movie->getTitle() . " " . $rental->getCharge() . "<br />\n";
@@ -54,7 +59,7 @@ class Customer {
         return $result;
     }
     
-    public function getTotalFrequentRenterPoints() {
+    public function getTotalFrequentRenterPoints(): int {
         $result = 0;
         
         foreach ($this->rentals as $rental) {
@@ -64,7 +69,7 @@ class Customer {
         return $result;
     }
     
-    public function getTotalCharge() {
+    public function getTotalCharge(): float {
         $result = 0;
         
         foreach ($this->rentals as $rental) {
@@ -76,19 +81,19 @@ class Customer {
 }
 
 class Movie {
-    const CHILDRENS = 2;
+    const CHILDREN = 2;
     const REGULAR = 0;
     const NEW_RELEASE = 1;
-    
-    public $title;
-    public $priceCode;
+
+    protected string $title;
+    protected int $priceCode;
     
     public function __construct($title, $priceCode) {
         $this->title = $title;
         $this->setPriceCode($priceCode);
     }
     
-    public function getPriceCode() {
+    public function getPriceCode(): int {
         return $this->priceCode;
     }
     
@@ -96,29 +101,29 @@ class Movie {
         $this->priceCode = $priceCode;
     }
     
-    public function getTitle() {
+    public function getTitle(): string {
         return $this->title;
     }
 }
 
 class Rental {
-    public $movie;
-    public $daysRented;
+    public Movie $movie;
+    protected int $daysRented;
     
     public function __construct(Movie $movie, $daysRented) {
         $this->movie = $movie;
         $this->daysRented = $daysRented;
     }
     
-    public function getDaysRented() {
+    public function getDaysRented(): int {
         return $this->daysRented;
     }
     
-    public function getMovie() {
+    public function getMovie(): Movie {
         return $this->movie;
     }
 
-    public function getCharge() {
+    public function getCharge(): float {
         $result = 0;
 
         switch ($this->movie->getPriceCode()) {
@@ -133,7 +138,7 @@ class Rental {
                 $result += $this->getDaysRented() * 3;
                 break;
 
-            case Movie::CHILDRENS:
+            case Movie::CHILDREN:
                 $result += 1.5;
                 if ($this->getDaysRented() > 3) {
                     $result += ($this->getDaysRented() - 3) * 1.5;
@@ -144,13 +149,17 @@ class Rental {
         return $result;
     }
 
-    public function getFrequentRenterPoints() {
-        // add bonus for a two day release rental
+    public function getFrequentRenterPoints(): int {
+        $result = 0;
+
+        // add bonus for a two-day new release rental
         if (($this->movie->getPriceCode() == Movie::NEW_RELEASE) && ($this->getDaysRented() > 1)) {
-            return 2;
+            $result += 2;
         } else {
-            return 1;
+            $result += 1;
         }
+
+        return $result;
     }
 }
 
